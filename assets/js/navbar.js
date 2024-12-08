b498569f31d0192bcba2b7afe8e90d084937c006
Achetious <chazpascual62@gmail.com>
2024-12-08 21:06:03 +0800

    latest

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,16 @@
 // Function to toggle responsive menu
 document.getElementById("burgerBtn").addEventListener("click", function () {
-  document
-    .getElementById("responsiveMenu")
-    .classList.toggle("-translate-x-full");
+  var menu = document.getElementById("responsiveMenu");
+  var mainContent = document.querySelector(".main-content");
+
+  menu.classList.toggle("-translate-x-full");
+
+  // Toggle blur effect on .main-content
+  if (!menu.classList.contains("-translate-x-full")) {
+    mainContent.classList.add("blur");
+  } else {
+    mainContent.classList.remove("blur");
+  }
 });
 
 var hamburgers = document.querySelectorAll(".hamburger");
@@ -17,3 +25,25 @@ if (hamburgers.length > 0) {
     );
   });
 }
+
+// Function to close sidebar when clicking outside
+function closeSidebarOnClickOutside(event) {
+  var sidebar = document.getElementById("responsiveMenu");
+  var burgerBtn = document.getElementById("burgerBtn");
+  var mainContent = document.querySelector(".main-content");
+
+  // Check if the clicked element is outside the sidebar and burger button
+  if (
+    !sidebar.contains(event.target) &&
+    event.target !== burgerBtn &&
+    !burgerBtn.contains(event.target)
+  ) {
+    sidebar.classList.add("-translate-x-full");
+    burgerBtn.classList.remove("is-active"); // Remove is-active class from hamburger icon
+    mainContent.classList.remove("blur"); // Remove blur effect from .main-content
+    return;
+  }
+}
+
+// Add event listener to close sidebar on click outside
+document.body.addEventListener("click", closeSidebarOnClickOutside);
